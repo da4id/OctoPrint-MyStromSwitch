72de3c4db6d23babf1fb5fac50b35408075eec1b
David Zingg <mail@davidzingg.ch>
2020-07-01 20:49:11 +0200

    #21 Add PowerOFF when print is finished

--- octoprint_mystromswitch/__init__.py
+++ octoprint_mystromswitch/__init__.py
@@ -279,28 +279,28 @@ class MyStromSwitchPlugin(octoprint.plugin.SettingsPlugin,
             self.shutdownAfterPrintFinished = True
             if self.rememberShutdown:
                 self.lastShutdown = self.shutdownAfterPrintFinished
-                self._settings.set_boolean("lastShutdown", self.lastShutdown)
+                self._settings.set_boolean(["lastShutdown"], self.lastShutdown)
                 self._logger.info("lastShutdown: %s" % self.lastShutdown)
         elif command == "disableShutdownAfterFinish":
             self._logger.info("disableShutdownAfterFinish")
             self.shutdownAfterPrintFinished = False
             if self.rememberShutdown:
                 self.lastShutdown = self.shutdownAfterPrintFinished
-                self._settings.set_boolean("lastShutdown", self.lastShutdown)
+                self._settings.set_boolean(["lastShutdown"], self.lastShutdown)
                 self._logger.info("lastShutdown: %s" % self.lastShutdown)
         elif command == "enablePowerOffAfterFinish":
             self._logger.info("enablePowerOffAfterFinish")
             self.powerOffAfterPrintFinished = True
             if self.rememberShutdown:
                 self.lastPowerOff = self.powerOffAfterPrintFinished
-                self._settings.set_boolean("lastPowerOff", self.lastPowerOff)
+                self._settings.set_boolean(["lastPowerOff"], self.lastPowerOff)
                 self._logger.info("lastPowerOff: %s" % self.lastPowerOff)
         elif command == "disablePowerOffAfterFinish":
             self._logger.info("disablePowerOffAfterFinish")
             self.powerOffAfterPrintFinished = False
             if self.rememberShutdown:
                 self.lastPowerOff = self.powerOffAfterPrintFinished
-                self._settings.set_boolean("lastPowerOff", self.lastPowerOff)
+                self._settings.set_boolean(["lastPowerOff"], self.lastPowerOff)
                 self._logger.info("lastPowerOff: %s" % self.lastPowerOff)
 
     def get_api_commands(self):
